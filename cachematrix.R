e94a79e46ec56a42b5671f3096fd7446776df04f
saish29 <saishmandavkar@live.com>
2020-05-24 02:37:04 +0530

    Create, get and set Matrix with get and set inverse functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A couple of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## Create a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## Inverse property initialization 
+  
+  i <- NULL
+  
+  ## Setting the Matrix
+  
+  setMatrix <- function(matrix) {
+    
+    m <<- matrix
+    i <<- NULL
+    
+    
+  }
+  
+  ## Getting the Matrix
+  
+  getMatrix <- function(matrix) {
+    
+    ## Return matrix
+    m
+  }
+  
+  ## Inversing Matrix function
+  
+  setInverse <- function(inverse) {
+    i <<- inverse
+    
+  }
+  
+  ## Getting the Inverse Matrix function
+  
+  getInverse <- function() {
+    # Returning inverse property
+    i
+  }
+  
+  ## Return all the methods in a list
+  
+  Flist(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
+  
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
